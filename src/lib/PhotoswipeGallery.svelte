ea7d9366c2b3c4a290c17dd4dcadefa7b0e37677
Fankhauser-Dominik <domi.fankhauser@hotmail.com>
2023-07-12 01:01:41 +0200

    Added a LoadingSpinner

--- src/lib/PhotoswipeGallery.svelte
+++ src/lib/PhotoswipeGallery.svelte
@@ -1,157 +1,189 @@
 <script lang="ts">
-import {
-    onMount
-} from "svelte";
-import {
-    tick
-} from "svelte";
+	import { afterUpdate, onMount, tick } from 'svelte';
 
-import PhotoSwipeLightbox from './Photoswipe/photoswipe-lightbox.esm.js';
-import PhotoSwipeFullscreen from './Photoswipe/photoswipe-fullscreen.esm.js';
-import './Photoswipe/photoswipe.css';
-import './styles.css';
-export let galleryID: string = "Svelte-Photoswipe-Gallery";
-export let gap: number = 10;
-export let maxColumnWidth: number = 250;
-export let hover: boolean = false;
-export let loading: "lazy" | "eager" | null | undefined = "lazy";
-export let images: any[] = [];
-export let showDescription: boolean = true;
-export let showDownloadButton: boolean = true;
-export let showFullscreenButton: boolean = true;
+	import PhotoSwipeLightbox from './Photoswipe/photoswipe-lightbox.esm.js';
+	import PhotoSwipeFullscreen from './Photoswipe/photoswipe-fullscreen.esm.js';
+	import './Photoswipe/photoswipe.css';
+	import './styles.css';
+	export let galleryID: string = 'Svelte-Photoswipe-Gallery';
+	export let gap: number = 10;
+	export let maxColumnWidth: number = 250;
+	export let hover: boolean = false;
+	export let loading: 'lazy' | 'eager' | null | undefined = 'lazy';
+	export let images: any[] = [];
+	export let showDescription: boolean = true;
+	export let showDownloadButton: boolean = true;
+	export let showFullscreenButton: boolean = true;
+    export let noLoadingSpinner: boolean = true;
 
-let columns: any[] = [];
-let galleryWidth: number = 0;
-let columnCount: number = 0;
+	let columns: any[] = [];
+	let galleryWidth: number = 0;
+	let columnCount: number = 0;
 
-$: columnCount = parseInt(galleryWidth / maxColumnWidth) || 1;
-$: columnCount && Draw();
-$: galleryStyle =
-    "grid-template-columns: repeat(" +
-    columnCount +
-    ", 1fr); --gap: " +
-    gap +
-    "px";
-onMount(Draw);
+	$: columnCount = parseInt(`${galleryWidth / maxColumnWidth}`) || 1;
+	$: columnCount && Draw();
+	$: galleryStyle = 'grid-template-columns: repeat(' + columnCount + ', 1fr); --gap: ' + gap + 'px';
+	onMount(Draw);
 
-async function Draw() {
-    await tick();
+	async function Draw() {
+		await tick();
 
-    if (!images) {
-        return;
-    }
+		if (!images) {
+			return;
+		}
 
-    columns = []
-    // Fill the columns with image URLs
-    for (let i = 0; i < images.length; i++) {
-        const idx = i % columnCount;
-        columns[idx] = [
-            ...(columns[idx] || []),
-            {
-                src: images[i].src,
-                preview: images[i].preview,
-                description: images[i].description,
-                width: images[i].width,
-                height: images[i].height,
-                alt: images[i].alt,
-                class: images[i].className,
-                index: i
-            },
-        ];
-    }
-}
+		columns = [];
+		// Fill the columns with image URLs
+		for (let i = 0; i < images.length; i++) {
+			const idx = i % columnCount;
+			columns[idx] = [
+				...(columns[idx] || []),
+				{
+					src: images[i].src,
+					preview: images[i].preview,
+					description: images[i].description,
+					width: images[i].width,
+					height: images[i].height,
+					alt: images[i].alt,
+					class: images[i].className,
+					index: i
+				}
+			];
+		}
+	}
 
-let lightbox = new PhotoSwipeLightbox({
-    gallery: '#' + galleryID,
-    // children: 'a',
-    pswpModule: () => import('$lib/Photoswipe/photoswipe.esm.js'),
-    dataSource: images,
-});
+	let lightbox = new PhotoSwipeLightbox({
+		gallery: '#' + galleryID,
+		// children: 'a',
+		pswpModule: () => import('$lib/Photoswipe/photoswipe.esm.js'),
+		dataSource: images
+	});
 
-lightbox.on('uiRegister', function() {
-    if (showDescription) {
-        lightbox.pswp.ui.registerElement({
-            name: 'custom-caption',
-            order: 9,
-            isButton: false,
-            appendTo: 'root',
-            html: 'Caption text',
-            onInit: (el: any) => {
-                lightbox.pswp.on('change', () => {
-                    const currSlideElement = lightbox.pswp.currSlide.data;
-                    let alt = '';
-                    let description = '';
-                    if (currSlideElement) {
-                        alt = currSlideElement.alt;
-                        description = currSlideElement.description;
-                    }
-                    el.innerHTML = description ?? alt ?? '';
-                });
-            }
-        });
-    }
-});
+	lightbox.on('uiRegister', function () {
+		if (showDescription) {
+			lightbox.pswp.ui.registerElement({
+				name: 'custom-caption',
+				order: 9,
+				isButton: false,
+				appendTo: 'root',
+				html: 'Caption text',
+				onInit: (el: any) => {
+					lightbox.pswp.on('change', () => {
+						const currSlideElement = lightbox.pswp.currSlide.data;
+						let alt = '';
+						let description = '';
+						if (currSlideElement) {
+							alt = currSlideElement.alt;
+							description = currSlideElement.description;
+						}
+						el.innerHTML = description ?? alt ?? '';
+					});
+				}
+			});
+		}
+	});
 
-if (showDownloadButton) {
-    lightbox.on('uiRegister', function() {
-        lightbox.pswp.ui.registerElement({
-            name: 'download-button',
-            order: 8,
-            isButton: true,
-            tagName: 'a',
+	if (showDownloadButton) {
+		lightbox.on('uiRegister', function () {
+			lightbox.pswp.ui.registerElement({
+				name: 'download-button',
+				order: 8,
+				isButton: true,
+				tagName: 'a',
 
-            html: {
-                isCustomSVG: true,
-                inner: '<path d="M20.5 14.3 17.1 18V10h-2.2v7.9l-3.4-3.6L10 16l6 6.1 6-6.1ZM23 23H9v2h14Z" id="pswp__icn-download"/>',
-                outlineID: 'pswp__icn-download'
-            },
+				html: {
+					isCustomSVG: true,
+					inner:
+						'<path d="M20.5 14.3 17.1 18V10h-2.2v7.9l-3.4-3.6L10 16l6 6.1 6-6.1ZM23 23H9v2h14Z" id="pswp__icn-download"/>',
+					outlineID: 'pswp__icn-download'
+				},
 
-            onInit: (el: any, pswp: any) => {
-                el.setAttribute('download', '');
-                el.setAttribute('target', '_blank');
-                el.setAttribute('rel', 'noopener');
+				onInit: (el: any, pswp: any) => {
+					el.setAttribute('download', '');
+					el.setAttribute('target', '_blank');
+					el.setAttribute('rel', 'noopener');
 
-                pswp.on('change', () => {
-                    el.href = pswp.currSlide.data.src;
-                });
-            }
-        });
-    });
-}
+					pswp.on('change', () => {
+						el.href = pswp.currSlide.data.src;
+					});
+				}
+			});
+		});
+	}
 
-if (showFullscreenButton) {
-    let fullscreenPlugin = new PhotoSwipeFullscreen(lightbox);
-}
+	if (showFullscreenButton) {
+		let fullscreenPlugin = new PhotoSwipeFullscreen(lightbox);
+	}
 
-lightbox.on('close', function() {
-    lightbox.init();
-});
+	lightbox.on('close', function () {
+		lightbox.init();
+	});
 
-onMount(() => {
-    lightbox.init();
-    return () => {
-        lightbox.destroy();
-    };
-});
+	onMount(() => {
+		lightbox.init();
+		return () => {
+			lightbox.destroy();
+		};
+	});
 
-const onClick = (index: number) => {
-    lightbox.loadAndOpen(index);
-}
+	const onClick = (index: number) => {
+		lightbox.loadAndOpen(index);
+	};
+
+	let isLoading = true;
+
+	afterUpdate(() => {
+		isLoading = false;
+	});
 </script>
 
-<div id="gallery" bind:clientWidth={galleryWidth} style={galleryStyle}>
-    {#each columns as column}
-    <div class="column">
-        {#each column as img, index}
-        <img
-            src={img.preview ?? img.src}
-            alt={img.alt}
-            class="{hover === true ? 'img-hover' : ''} {img.class} GalleryImage"
-            {loading}
-            on:click={() => onClick(img.index)}
-            on:keypress={() => onClick(img.index)}
-        />
-        {/each}
-    </div>
-    {/each}
-</div>
+{#if isLoading && !noLoadingSpinner}
+	<div class="spinnerContainer">
+		<div class="spinner" />
+	</div>
+{:else}
+	<div id="gallery" bind:clientWidth={galleryWidth} style={galleryStyle}>
+		{#each columns as column}
+			<div class="column">
+				{#each column as img, index}
+					<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+					<img
+						src={img.preview ?? img.src}
+						alt={img.alt}
+						class="{hover === true ? 'img-hover' : ''} {img.class} GalleryImage"
+						{loading}
+						on:click={() => onClick(img.index)}
+						on:keypress={() => onClick(img.index)}
+					/>
+				{/each}
+			</div>
+		{/each}
+	</div>
+{/if}
+
+<style>
+	.spinnerContainer {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 100%;
+		width: 100%;
+	}
+	.spinner {
+		border: 6px solid #f3f3f3; /* Light grey */
+		border-top: 6px solid #3498db; /* Blue */
+		border-radius: 50%;
+		width: 48px;
+		height: 48px;
+		animation: spin 1s linear infinite;
+	}
+
+	@keyframes spin {
+		0% {
+			transform: rotate(0deg);
+		}
+		100% {
+			transform: rotate(360deg);
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,211 +1,211 @@
 <script lang="ts">
-    import PhotoswipeGallery from "$lib/PhotoswipeGallery.svelte"
+	import PhotoswipeGallery from '$lib/PhotoswipeGallery.svelte';
 
-    const images = [
-  {
-    src: "https://placebear.com/1920/1080",
-    height: 1080,
-    width: 1920,
-    alt: "Test",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-  },
-  {
-    src: "https://placebear.com/600/400",
-    width: 600,
-    height: 400,
-    description: '<a href="https://google.com">Google</a>',
-  },
-  {
-    src: "https://placebear.com/400/400",
-    width: 4000,
-    height: 4000,
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit2",
-    alt: "Random",
-  },
-  {
-    src: "https://placebear.com/1920/1080",
-    preview: "https://placebear.com/400/400",
-    width: 1920,
-    height: 1080,
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit3",
-    alt: "Random",
-  },
-  {
-    src: "https://source.unsplash.com/random",
-    width: 300,
-    height: 200,
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit4",
-    alt: "Random",
-  },
-  {
-    src: "https://source.unsplash.com/random",
-    width: 600,
-    height: 400,
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit5",
-    alt: "Random",
-  },
-  {
-    src: "https://placebear.com/600/400",
-    width: 600,
-    height: 400,
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit1",
-  },
-  {
-    src: "https://source.unsplash.com/random",
-    width: 800,
-    height: 1200,
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    alt: "Random",
-  },
-  {
-    src: "https://source.unsplash.com/random",
-    width: 300,
-    height: 200,
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    alt: "Random",
-  },
-  {
-    src: "https://source.unsplash.com/random",
-    width: 600,
-    height: 400,
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    alt: "Random",
-  },
-  {
-    src: "https://source.unsplash.com/random",
-    width: 400,
-    height: 600,
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    alt: "Random",
-  },
-  {
-    src: "https://source.unsplash.com/random",
-    width: 800,
-    height: 1200,
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    alt: "Random",
-  },
-  {
-    src: "https://source.unsplash.com/random",
-    width: 300,
-    height: 200,
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    alt: "Random",
-  },
-  {
-    src: "https://source.unsplash.com/random",
-    width: 600,
-    height: 400,
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    alt: "Random",
-  },
-  {
-    src: "https://source.unsplash.com/random",
-    width: 400,
-    height: 600,
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    alt: "Random",
-  },
-  {
-    src: "https://source.unsplash.com/random",
-    width: 800,
-    height: 1200,
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    alt: "Random",
-  },
-  {
-    src: "https://source.unsplash.com/random",
-    width: 300,
-    height: 200,
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    alt: "Random",
-  },
-  {
-    src: "https://source.unsplash.com/random",
-    width: 600,
-    height: 400,
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    alt: "Random",
-  },
-  {
-    src: "https://source.unsplash.com/random",
-    width: 400,
-    height: 600,
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    alt: "Random",
-  },
-  {
-    src: "https://source.unsplash.com/random",
-    width: 800,
-    height: 1200,
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    alt: "Random",
-  },
-  {
-    src: "https://source.unsplash.com/random",
-    width: 300,
-    height: 200,
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    alt: "Random",
-  },
-  {
-    src: "https://source.unsplash.com/random",
-    width: 600,
-    height: 400,
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    alt: "Random",
-  },
-  {
-    src: "https://source.unsplash.com/random",
-    width: 400,
-    height: 600,
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    alt: "Random",
-  },
-  {
-    src: "https://source.unsplash.com/random",
-    width: 800,
-    height: 1200,
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    alt: "Random",
-  },
-  {
-    src: "https://source.unsplash.com/random",
-    width: 300,
-    height: 200,
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    alt: "Random",
-  },
-  {
-    src: "https://source.unsplash.com/random",
-    width: 600,
-    height: 400,
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    alt: "Random",
-  },
-  {
-    src: "https://source.unsplash.com/random",
-    width: 400,
-    height: 600,
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    alt: "Random",
-  },
-  {
-    src: "https://source.unsplash.com/random",
-    width: 800,
-    height: 1200,
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    alt: "Random",
-  },
-  {
-    src: "https://source.unsplash.com/random",
-    width: 300,
-    height: 200,
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
-    alt: "Random",
-  },
-];
+	const images = [
+		{
+			src: 'https://placebear.com/1920/1080',
+			height: 1080,
+			width: 1920,
+			alt: 'Test',
+			description:
+				'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+		},
+		{
+			src: 'https://placebear.com/600/400',
+			width: 600,
+			height: 400,
+			description: '<a href="https://google.com">Google</a>'
+		},
+		{
+			src: 'https://placebear.com/400/400',
+			width: 4000,
+			height: 4000,
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit2',
+			alt: 'Random'
+		},
+		{
+			src: 'https://placebear.com/1920/1080',
+			preview: 'https://placebear.com/400/400',
+			width: 1920,
+			height: 1080,
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit3',
+			alt: 'Random'
+		},
+		{
+			src: 'https://source.unsplash.com/random',
+			width: 300,
+			height: 200,
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit4',
+			alt: 'Random'
+		},
+		{
+			src: 'https://source.unsplash.com/random',
+			width: 600,
+			height: 400,
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit5',
+			alt: 'Random'
+		},
+		{
+			src: 'https://placebear.com/600/400',
+			width: 600,
+			height: 400,
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit1'
+		},
+		{
+			src: 'https://source.unsplash.com/random',
+			width: 800,
+			height: 1200,
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+			alt: 'Random'
+		},
+		{
+			src: 'https://source.unsplash.com/random',
+			width: 300,
+			height: 200,
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+			alt: 'Random'
+		},
+		{
+			src: 'https://source.unsplash.com/random',
+			width: 600,
+			height: 400,
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+			alt: 'Random'
+		},
+		{
+			src: 'https://source.unsplash.com/random',
+			width: 400,
+			height: 600,
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+			alt: 'Random'
+		},
+		{
+			src: 'https://source.unsplash.com/random',
+			width: 800,
+			height: 1200,
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+			alt: 'Random'
+		},
+		{
+			src: 'https://source.unsplash.com/random',
+			width: 300,
+			height: 200,
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+			alt: 'Random'
+		},
+		{
+			src: 'https://source.unsplash.com/random',
+			width: 600,
+			height: 400,
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+			alt: 'Random'
+		},
+		{
+			src: 'https://source.unsplash.com/random',
+			width: 400,
+			height: 600,
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+			alt: 'Random'
+		},
+		{
+			src: 'https://source.unsplash.com/random',
+			width: 800,
+			height: 1200,
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+			alt: 'Random'
+		},
+		{
+			src: 'https://source.unsplash.com/random',
+			width: 300,
+			height: 200,
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+			alt: 'Random'
+		},
+		{
+			src: 'https://source.unsplash.com/random',
+			width: 600,
+			height: 400,
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+			alt: 'Random'
+		},
+		{
+			src: 'https://source.unsplash.com/random',
+			width: 400,
+			height: 600,
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+			alt: 'Random'
+		},
+		{
+			src: 'https://source.unsplash.com/random',
+			width: 800,
+			height: 1200,
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+			alt: 'Random'
+		},
+		{
+			src: 'https://source.unsplash.com/random',
+			width: 300,
+			height: 200,
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+			alt: 'Random'
+		},
+		{
+			src: 'https://source.unsplash.com/random',
+			width: 600,
+			height: 400,
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+			alt: 'Random'
+		},
+		{
+			src: 'https://source.unsplash.com/random',
+			width: 400,
+			height: 600,
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+			alt: 'Random'
+		},
+		{
+			src: 'https://source.unsplash.com/random',
+			width: 800,
+			height: 1200,
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+			alt: 'Random'
+		},
+		{
+			src: 'https://source.unsplash.com/random',
+			width: 300,
+			height: 200,
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+			alt: 'Random'
+		},
+		{
+			src: 'https://source.unsplash.com/random',
+			width: 600,
+			height: 400,
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+			alt: 'Random'
+		},
+		{
+			src: 'https://source.unsplash.com/random',
+			width: 400,
+			height: 600,
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+			alt: 'Random'
+		},
+		{
+			src: 'https://source.unsplash.com/random',
+			width: 800,
+			height: 1200,
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+			alt: 'Random'
+		},
+		{
+			src: 'https://source.unsplash.com/random',
+			width: 300,
+			height: 200,
+			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+			alt: 'Random'
+		}
+	];
 </script>
 
-  <PhotoswipeGallery {images} />
-  
+<PhotoswipeGallery {images} />
